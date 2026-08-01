5e64a276d2847af0348d68553691eb536435ac36
25cs578-Janani <25cs578@skcet.ac.in>
2026-08-01 23:56:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let message =
+"Thank you for always being there for me. ❤️ Happy Friendship Day! May our friendship stay forever. 🌸";
+
+function unlock(){
+
+let pass=document.getElementById("password").value;
+
+if(pass=="friends2026"){
+
+document.getElementById("lockScreen").style.display="none";
+
+let c=document.getElementById("countdown");
+
+c.style.display="block";
+
+let n=3;
+
+let timer=setInterval(()=>{
+
+c.innerHTML=n;
+
+n--;
+
+if(n<0){
+
+clearInterval(timer);
+
+c.style.display="none";
+
+document.getElementById("main").style.display="flex";
+
+type();
+
+document.getElementById("photo").style.display="block";
+
+}
+
+},1000);
+
+}
+else{
+
+document.getElementById("error").innerHTML="Wrong Password ❤️";
+
+}
+
+}
+
+function type(){
+
+let i=0;
+
+let interval=setInterval(()=>{
+
+document.getElementById("typing").innerHTML+=message.charAt(i);
+
+i++;
+
+if(i==message.length)
+clearInterval(interval);
+
+},45);
+
+}
+
+for(let i=0;i<40;i++){
+
+let h=document.createElement("div");
+
+h.className="heart";
+
+h.innerHTML="💖";
+
+h.style.left=Math.random()*100+"%";
+
+h.style.fontSize=(20+Math.random()*30)+"px";
+
+h.style.animationDuration=(5+Math.random()*6)+"s";
+
+document.body.appendChild(h);
+
+}
